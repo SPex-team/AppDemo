158ebe95eb640c9e432014942ef34b5b0a6e339d
mingming.tang <mingmingtang@aliyun.com>
2023-02-06 18:22:53 +0800

    v0.1.1

--- src/config/pro.js
+++ src/config/pro.js
@@ -1,4 +1,6 @@
 
 export default {
-    base_url: window.location.protocol + "//" + window.location.host + "/api/v1/"
+    base_url: window.location.protocol + "//" + window.location.host + "/api/v1/",
+    contractAddress: "0xb81dFE627379bD869179Eb19dc1682c36EbAE2a0",
+    contractT4Address: "t410fxao74yttpg6ynelz5mm5yfucynxlvyva2toihli"
 }
