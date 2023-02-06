173d29961c49c7f6c983a4c4a278859281bea19e
mingming.tang <mingmingtang@aliyun.com>
2023-02-06 18:05:32 +0800

    add static files

--- src/views/tests/Test1.vue
+++ src/views/tests/Test1.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Test1</h1>
     <el-button @click="onTest">test</el-button>
+    <el-button @click="onTest2">test2</el-button>
   </div>
 </template>
 
@@ -9,35 +10,133 @@
 
 import axios from "axios";
 
+
+import fa from "@glif/filecoin-address"
+import {ethers} from "ethers";
+import config from "@/config";
+import abi from "@/others/abi";
+
+
 export default {
   name: "Test1",
   methods: {
-    onTest() {
+    async onTest() {
+      const abi = [
+        {
+          "inputs": [
+            {
+              "internalType": "uint64",
+              "name": "target",
+              "type": "uint64"
+            }
+          ],
+          "name": "getOwner",
+          "outputs": [
+            {
+              "internalType": "bytes",
+              "name": "",
+              "type": "bytes"
+            },
+            {
+              "internalType": "bytes",
+              "name": "",
+              "type": "bytes"
+            }
+          ],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint64",
+              "name": "target",
+              "type": "uint64"
+            }
+          ],
+          "name": "getOwner2",
+          "outputs": [
+            {
+              "components": [
+                {
+                  "internalType": "bytes",
+                  "name": "owner",
+                  "type": "bytes"
+                },
+                {
+                  "internalType": "bytes",
+                  "name": "proposed",
+                  "type": "bytes"
+                }
+              ],
+              "internalType": "struct MinerTypes.GetOwnerReturn",
+              "name": "",
+              "type": "tuple"
+            }
+          ],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "bytes",
+              "name": "target",
+              "type": "bytes"
+            }
+          ],
+          "name": "getSectorSize",
+          "outputs": [
+            {
+              "internalType": "uint64",
+              "name": "",
+              "type": "uint64"
+            }
+          ],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "bytes",
+              "name": "target",
+              "type": "bytes"
+            },
+            {
+              "internalType": "bytes",
+              "name": "addr",
+              "type": "bytes"
+            }
+          ],
+          "name": "transferOwnerOut",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        }
+      ]
 
-      // this.$api.get("spex/miners").then(response=>{
-      //   console.log("response: ", response)
-      // })
+      const addresses = await window.ethereum.request({method: 'eth_requestAccounts'})
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract("0x37aB12B8415703E5DeEE2Cc13b5A0E87e526a682", abi, signer)
+      let tx = await contract.getOwner("0x00da43", {gasLimit: 100000000})
+      // let tx = await contract.getSectorSize("0x00da43", {gasLimit: 100000000})
+      this.$message.info(`Waiting transaction: ${tx.hash}`)
+      console.log("tx: ", tx)
+      let result = await tx.wait()
+      console.log("result: ", result)
+    },
+    onTest2() {
+      const minerId = fa.newFromString("t08666");
+      console.log(
+          "minerId :", minerId.toString(),
+          "bytes: ", minerId.bytes,
+          "hex addr:", Buffer.from(minerId.bytes).toString("hex")
+      );
+      console.log("Buffer.from(minerId.bytes).toString(\"hex\"): ", Buffer.from(minerId.bytes).toString("hex"))
 
-      // const instance = axios.create({
-      //   baseURL: 'https://some-domain.com/api/',
-      //   timeout: 1000,
-      //   headers: {'X-Custom-Header': 'foobar'}
-      // });
-      //
-      //
-      // console.log("instance: ", instance)
-      // console.log("this.$api: ", this.$api)
-      //
-      // instance.get("spex/miners").then(response => {
-      //   console.log("response: ", response)
-      // })
-
-      console.log("1")
-
-      this.$backend.get("/spex/miners").then(response => {
-        console.log("response: ", response)
-      })
-
+// minerId : t01000 ,bytes:  Uint8Array(3) [ 0, 232, 7 ] hex addr: 00e807
     }
   }
 }
